5f3fa001607e927e24e657fe08717a6dd5cb0e1b
youssefassad0 <amine150assad@gmail.com>
2024-07-08 19:08:36 +0100

    make the structure

--- my-app/src/components/hero/Hero.jsx
+++ my-app/src/components/hero/Hero.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-import './hero.css'
+import "./hero.css";
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="hero flex">
+      <div className="left-section border">
+        <div className="parent-avatar">
+          <img src="assad.png" className="avatar" alt="" />
+          <span className="" ></span>
+        </div>
+      </div>
+      <div className="right-section animation border">animation</div>
+    </section>
+  );
 }
 
 export default Hero;
