7fb0301b4cfceb803df4ab4a2823da9960d6f043
youssefassad0 <amine150assad@gmail.com>
2024-07-09 00:11:48 +0100

    post icons

--- my-app/src/components/hero/Hero.jsx
+++ my-app/src/components/hero/Hero.jsx
@@ -6,7 +6,22 @@ function Hero() {
       <div className="left-section">
         <div className="parent-avatar">
           <img src="image_assad1.png" className="avatar" alt="" />
-          <span className="" ></span>
+          <span className=""></span>
+        </div>
+        <h1 className="title">
+          Full Stack developer , ambitious and passionate
+        </h1>
+        <p className="sub-title" >
+          Ambitious and passionate about digital development, my goal is to
+          become a full stack option engineer. I seeks to put into practice the
+          skills I have acquired and to continue learning to contribute
+          significantly to innovative and complex projects.
+        </p>
+        <div className="all-icons flex">
+          <div className="icon icon-github"></div>
+          <div className="icon icon-linkedin-square"></div>
+          <div className="icon icon-instagram"></div>
+          <div className="icon icon-skype"></div>
         </div>
       </div>
       <div className="right-section animation border">animation</div>
