a8b0e22cea939592d6eda3af40dff7813d0c6cc4
youssefassad0 <amine150assad@gmail.com>
2024-07-08 15:24:23 +0100

    adding icons and modal

--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 function Header() {
+  const [showModal,setShowModal]=useState(false);
   return (
-    <header className="flex" >
+    <header className="flex">
+      <button onClick={()=>setShowModal(true)} className="menu">Show Modal</button>
       <div />
       <nav>
         <ul className="flex">
-          <li><Link to="/about">about</Link></li>
-          <li><Link to="/">Articles</Link></li>
-          <li><Link to="/">Projects</Link></li>
-          <li><Link to="/about">Contact</Link></li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+          <li>
+            <Link to="/">Articles</Link>
+          </li>
+          <li>
+            <Link to="/">Projects</Link>
+          </li>
+          <li>
+            <Link to="/about">Contact</Link>
+          </li>
         </ul>
       </nav>
-      <button >Light</button>
+
+      <button>Light</button>
+{
+  showModal && ( <div className="flexed">
+    <ul className="modal">
+      <li  >
+        <button onClick={()=>setShowModal(false)} >close</button>
+      </li>
+      <li>
+        <Link to="/about">about</Link>
+      </li>
+      <li>
+        <Link to="/">Articles</Link>
+      </li>
+      <li>
+        <Link to="/">Projects</Link>
+      </li>
+      <li>
+        <Link to="/about">Contact</Link>
+      </li>
+    </ul>
+  </div>)
+}
+     
     </header>
   );
 }
