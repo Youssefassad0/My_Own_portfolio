f5dc2b618a95e01ceedfaab41a6389955b6f4893
youssefassad0 <amine150assad@gmail.com>
2024-07-08 13:48:03 +0100

    post the structure

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,9 +3,11 @@ import Hero from "./components/hero/Hero";
 import Main from "./components/main/Main";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
       <Header />
       <div className="devider" />
@@ -17,6 +19,7 @@ function App() {
       <div className="devider" />
       <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./header.css";
+import { Link } from "react-router-dom";
 function Header() {
   return (
-  <header>
-    <nav>
-
-    </nav>
-    <button>
-      
-    </button>
-  </header>
-  )
+    <header className="flex" >
+      <div />
+      <nav>
+        <ul className="flex">
+          <li><Link to="/about">about</Link></li>
+          <li><Link to="/">Articles</Link></li>
+          <li><Link to="/">Projects</Link></li>
+          <li><Link to="/about">Contact</Link></li>
+        </ul>
+      </nav>
+      <button >Light</button>
+    </header>
+  );
 }
 
 export default Header;
