723a09c885a21eae97cf92e3f8179a90a9f7a8ae
youssefassad0 <amine150assad@gmail.com>
2024-07-10 01:49:21 +0100

    send message to email

--- my-app/src/components/contact/Contact.jsx
+++ my-app/src/components/contact/Contact.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./contact.css";
+import { useForm, ValidationError } from "@formspree/react";
 function Contact() {
+  const [state, handleSubmit] = useForm("meojqyyk");
+  if (state.succeeded) {
+alert("thanks for meesage me ")}
   return (
     <section className="contact-us">
       <h1 className="title">
@@ -11,30 +15,35 @@ function Contact() {
         Contact Me For More Information and Get Notified
       </p>
       <div className="flex">
-        <form>
-          <div className="flex" >
+        <form onSubmit={handleSubmit}>
+          <div className="flex">
             <label htmlFor="email">Email Adress :</label>
             <input
               type="email"
               id="email"
               name="email"
-         
               placeholder="Email Adress"
             />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
-          <div  className="flex" style={{ marginTop: "23px" }}  >
-            <label htmlFor="message">
-              Your Message :
-            </label>
-            <textarea name="message" id="message" required ></textarea>
+          <div className="flex" style={{ marginTop: "23px" }}>
+            <label htmlFor="message">Your Message :</label>
+            <textarea name="message" id="message" required></textarea>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </div>
 
-          <button className="submit" >Send</button>
+          <button className="submit" disabled={state.submitting} >Send</button>
         </form>
 
-        <div className="animation border">
-          animation
-        </div>
+        <div className="animation border">animation</div>
       </div>
     </section>
   );
